ae7c6eee68ebb4fe2243c46d1586c457b43d1077
Giovanni Palusa <giovanni@palusa.se>
2020-05-19 14:04:32 +0200

    added formatting of numbers in chart

--- Sources/PieCharts/Layer/Impl/Common/PieChartLabelSettings.swift
+++ Sources/PieCharts/Layer/Impl/Common/PieChartLabelSettings.swift
@@ -13,7 +13,13 @@ public class PieChartLabelSettings {
     public var bgColor: UIColor = UIColor.clear
     public var font: UIFont = UIFont.boldSystemFont(ofSize: 20)
 
-    public var textGenerator: (PieSlice, _ title: Bool) -> String = { return $1 ? $0.data.model.title : "\($0.data.model.value)" }
+    public var textGenerator: (PieSlice, _ title: Bool) -> String = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.minimumFractionDigits = 0
+        formatter.currencySymbol = ""
+        return ($1 ? $0.data.model.title : formatter.string(from: NSNumber(value: $0.data.model.value))) ?? "\($0.data.model.value)"
+    }
 
     // Optional custom label - when this is set presentations settings (textColor, etc.) are ignored
     public var labelGenerator: ((PieSlice) -> UILabel)?
